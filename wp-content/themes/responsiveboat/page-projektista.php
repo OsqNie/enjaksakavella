7faf8285b6795d46c8e0451fad1081900a6f0c34
Osku <oskar.niemenoja@astehelsinki.fi>
2016-04-26 19:26:27 +0300

    Lisätty uusi Projektista -sivu

---
+++ wp-content/themes/responsiveboat/page-projektista.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Template Name: Projektista
+ */
+get_header(); ?>
+
+<div class="clear"></div>
+
+</header> <!-- / END HOME SECTION  -->
+
+<div id="content" class="site-content">
+
+    
+    <div class="pagetext-wrapper">
+
+    <p class="subheader">Projektista</p>
+    <span>L</span>orem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla justo. Nam scelerisque dolor sem, ac facilisis turpis volutpat a. Fusce quis sagittis sapien. Etiam diam felis, blandit nec maximus in, consequat a mauris. Nam in tortor iaculis, iaculis odio vel, cursus justo. Mauris vulputate tortor in tortor condimentum, ac pellentesque dolor sollicitudin. Sed euismod magna commodo cursus lacinia. Suspendisse non convallis lorem, vel bibendum velit. Duis molestie orci at lectus vehicula pretium. Donec vitae scelerisque est. Vestibulum hendrerit lacus ac augue rhoncus gravida. Vestibulum a volutpat nunc. Integer sit amet mi a tellus mattis aliquet non at libero. Vestibulum dictum leo pharetra purus ultrices, eu dapibus sapien vestibulum.
+
+    </div>
+
+    <div class="imgwrapper">
+      <img class="aligncenter size-full wp-image-44" src="http://www.inkubio.fi/enjaksakavella/wp-content/uploads/2016/04/banner1.png" alt="banner1" width="2000" height="500" />
+      <span>
+        Ajatus taustalla
+      </span>
+    </div>
+
+    <div class="pagetext-wrapper">
+
+    <p class="subheader">Lisää</p>
+    <span>L</span>orem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla justo. Nam scelerisque dolor sem, ac facilisis turpis volutpat a. Fusce quis sagittis sapien. Etiam diam felis, blandit nec maximus in, consequat a mauris. Nam in tortor iaculis, iaculis odio vel, cursus justo. Mauris vulputate tortor in tortor condimentum, ac pellentesque dolor sollicitudin. Sed euismod magna commodo cursus lacinia. Suspendisse non convallis lorem, vel bibendum velit. Duis molestie orci at lectus vehicula pretium. Donec vitae scelerisque est. Vestibulum hendrerit lacus ac augue rhoncus gravida. Vestibulum a volutpat nunc. Integer sit amet mi a tellus mattis aliquet non at libero. Vestibulum dictum leo pharetra purus ultrices, eu dapibus sapien vestibulum.
+    
+    </div>
+
+    <div class="imgwrapper">
+      <img class="aligncenter size-full wp-image-44" src="http://www.inkubio.fi/enjaksakavella/wp-content/uploads/2016/04/banner1.png" alt="banner1" width="2000" height="500" />
+      <span>
+        Tavoite
+      </span>
+    </div>
+
+    <div class="pagetext-wrapper">
+
+    <p class="subheader">Tekstiä</p>
+    <span>L</span>orem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla justo. Nam scelerisque dolor sem, ac facilisis turpis volutpat a. Fusce quis sagittis sapien. Etiam diam felis, blandit nec maximus in, consequat a mauris. Nam in tortor iaculis, iaculis odio vel, cursus justo. Mauris vulputate tortor in tortor condimentum, ac pellentesque dolor sollicitudin. Sed euismod magna commodo cursus lacinia. Suspendisse non convallis lorem, vel bibendum velit. Duis molestie orci at lectus vehicula pretium. Donec vitae scelerisque est. Vestibulum hendrerit lacus ac augue rhoncus gravida. Vestibulum a volutpat nunc. Integer sit amet mi a tellus mattis aliquet non at libero. Vestibulum dictum leo pharetra purus ultrices, eu dapibus sapien vestibulum.
+    
+    </div>
+
+    <div class="imgwrapper">
+      <img class="aligncenter size-full wp-image-44" src="http://www.inkubio.fi/enjaksakavella/wp-content/uploads/2016/04/banner1.png" alt="banner1" width="2000" height="500" />
+      <span>
+        Mitä odotettavissa
+      </span>
+    </div>
+
+    <div class="pagetext-wrapper">
+
+    <p class="subheader">Vielä</p>
+    <span>L</span>orem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla justo. Nam scelerisque dolor sem, ac facilisis turpis volutpat a. Fusce quis sagittis sapien. Etiam diam felis, blandit nec maximus in, consequat a mauris. Nam in tortor iaculis, iaculis odio vel, cursus justo. Mauris vulputate tortor in tortor condimentum, ac pellentesque dolor sollicitudin. Sed euismod magna commodo cursus lacinia. Suspendisse non convallis lorem, vel bibendum velit. Duis molestie orci at lectus vehicula pretium. Donec vitae scelerisque est. Vestibulum hendrerit lacus ac augue rhoncus gravida. Vestibulum a volutpat nunc. Integer sit amet mi a tellus mattis aliquet non at libero. Vestibulum dictum leo pharetra purus ultrices, eu dapibus sapien vestibulum.
+    
+    </div>
+
+
+<?php get_footer(); ?>
--- wp-content/themes/zerif-lite/template-fullwidth.php
+++ wp-content/themes/zerif-lite/template-fullwidth.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Full Width Page
+ * Template Name: Full width with no title template
  */
 get_header(); ?>
 
@@ -10,10 +10,33 @@ get_header(); ?>
 
 <div id="content" class="site-content">
 
-	<div class="container">
+  <div class="container">
 
-	Testiiiiii
+    <div class="content-left-wrap col-md-12">
 
-	</div><!-- .container -->
+      <div id="primary" class="content-area">
+
+        <main id="main" class="site-main" role="main">
+
+          <?php 
+            while ( have_posts() ) : the_post(); 
+            
+              get_template_part( 'content' );
+              
+              // If comments are open or we have at least one comment, load up the comment template
+              if ( comments_open() || '0' != get_comments_number() ) :
+                comments_template();
+              endif;
+              
+            endwhile;
+          ?>
+
+        </main><!-- #main -->
+
+      </div><!-- #primary -->
+
+    </div><!-- .content-left-wrap -->
+
+  </div><!-- .container -->
 
 <?php get_footer(); ?>
